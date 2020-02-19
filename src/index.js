6327dfaf345c655c40f05b20f9f97e413ce7ad0d
jhuang429 <jhuang9240@gmail.com>
2020-02-19 16:31:21 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let form = document.getElementById("create-task-form")
+  let textbox = document.getElementById("new-task-description")
+
+
+
+  form.addEventListener("submit", function(event){
+    addItem()
+    event.preventDefault()
+    textbox.value = ""
+  });
+
+  // deleteButtons.addEventListener("click", function(event){
+  //   //delete fucntion will go 
+  //   console.log("I was clicked")
+  //   // textbox.value = ""
+  // });
+
+
+
+
+  let list = document.getElementById("tasks")
+
+  let addItem = function () {
+    const deleteItem = function(){ button.parentElement.remove()}
+
+    let li = document.createElement("li")
+    let button = document.createElement("button")
+    button.innerText = "X"
+    button.className = "Delete"
+    li.innerText = textbox.value
+    list.append(li)
+    li.append(button)
+    button.addEventListener("click",function(event){deleteItem()
+    })
+  } 
+
 });
+
+
+
+
